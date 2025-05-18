7e852b199573bd4b1a123a66f65e3422251949be
Chris Baker <chrisbaker@Martins-iMac.local>
2025-05-18 14:51:02 -0400

    Added standardization and pre commit hook

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  const logger = new Logger('Bootstrap');
+  logger.log('Failed to start app: ' + error);
+});
